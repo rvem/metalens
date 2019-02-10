4566fac8682dfef65e6f97e3ab4cf1082c616fa8
rvem <roman.melnikov@serokell.io>
2019-02-10 19:43:27 +0300

    Some improvements
    -Add contidions to prevent particle sticking
    -Draw colormap after finding required metalens

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ import numpy as np
 
 import time
 
-X = np.arange(-200, 300, 100)
-Z = np.arange(6000, 10000, 50)
-Y = np.array([0])
-len_X = len(X)
-len_Z = len(Z)
 pi = np.pi
 
 EPS = 1
@@ -39,25 +34,36 @@ starts = np.array([0] * 15)
 chi = 4989276.07580808 + 1j * 15803067.7684502
 
 
-def main():
+def main(new_focus):
+    X = np.array([0])
+    Z = np.arange(1000, 10000, 50)
+    Y = np.array([0])
     np.random.seed(322)
     current_subject = Metalens(rings, starts, nums)
     it = 0
-    current_focus = calc(current_subject)
-    expecting = (0, 7550)
+    current_focus = calc(current_subject, X, Y, Z, False)
+    expecting = (0, new_focus)
     while it < 1000 and distance(current_focus, expecting) > EPS:
         new = mutate(current_subject)
-        focus = calc(new)
-        if distance(focus, expecting) < distance(current_focus, expecting):
+        focus = calc(new, X, Y, Z, False)
+        if distance(focus, expecting) < distance(current_focus, expecting) or\
+                (distance(focus, expecting) == distance(current_focus, expecting) and simpler(new, current_subject)):
             current_focus = focus
             current_subject = new
         it += 1
         print(it, focus, distance(focus, expecting))
         print()
-    draw_points(get_points(current_subject))
+    draw_points(get_points(current_subject), expecting, it)
+    X = np.arange(-2000, 2100, 100)
+    Z = np.arange(1000, 10050, 50)
+    print("drawing colormap")
+    draw_colormap(-2000, 2000, 1000, 10000, calc(current_subject, X, Y, Z, True), expecting)
+    return current_subject
 
 
-def calc(ring_subject: Metalens):
+def calc(ring_subject: Metalens, X, Y, Z, get_intensity: bool):
+    len_X = len(X)
+    len_Z = len(Z)
     dipoles = [Dipole(x, E_in * chi) for x in get_points(ring_subject)]
     n = len(dipoles)
     psi_0_2 = np.zeros((len_X, len_Z, 3), dtype=complex)
@@ -88,19 +94,24 @@ def calc(ring_subject: Metalens):
             psi_m[i, j] = psi_m[i, j] + s_m
 
     intensity = np.zeros((len_X, len_Z))
-    intensity_m = np.zeros((len_X, len_Z))
     for i in range(len_X):
         for j in range(len_Z):
             for t in range(3):
                 intensity[i, j] += np.abs(psi_2[i, j, t]) ** 2
-                intensity_m[i, j] += np.abs(psi_m[i, j, t]) ** 2
+                intensity[i, j] += np.abs(psi_m[i, j, t]) ** 2
 
-    m = np.transpose(intensity + intensity_m)
+    m = np.transpose(intensity)
     max_z, max_x = np.unravel_index(m.argmax(), m.shape)
-    return X[max_x], Z[max_z]
+    if get_intensity:
+        return intensity
+    else:
+        return X[max_x], Z[max_z]
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    main()
-    print(time.time() - start_time)
+    for i in range(6500, 7000, 100):
+        print("building lens with focus (0, {})".format(i))
+        start_time = time.time()
+        main(i)
+        print(time.time() - start_time)
+
--- metalens.py
+++ metalens.py
@@ -1,6 +1,8 @@
 import numpy as np
 from copy import copy
 
+mutations = ['radius', 'angle', 'number', 'add', 'remove']
+
 
 class Metalens:
     def __init__(self, rads, starts, nums):
@@ -12,18 +14,60 @@ class Metalens:
         return Metalens(copy(self.rads), copy(self.starts), copy(self.nums))
 
 
+def get_dist_to_closest_ring(new_rad, rads, index):
+    mn = np.inf
+    for i in range(len(rads)):
+        if abs(rads[i] - new_rad) < mn and i != index:
+            mn = abs(rads[i] - new_rad)
+    return mn
+
+
+def get_dist(num, rad):
+    res = np.sin(np.pi / num) * 2 * rad
+    return res
+
+
+def simpler(lens1: Metalens, lens2: Metalens):
+    return len(lens1.rads) < len(lens2.rads) or sum(lens2.nums) < sum(lens2.nums)
+
+
 def mutate(subject: Metalens):
     new_subject = copy(subject)
+    mutation = np.random.choice(mutations, 1, p=[0.3, 0.3, 0.3, 0.05, 0.05])
+    if mutation == 'add':
+        print("add new ring")
+        new_rad = np.random.randint(1000, 15000)
+        while get_dist_to_closest_ring(new_rad, new_subject.rads, -1) < 300:
+            new_rad = np.random.randint(1000, 15000)
+        new_angle = np.random.uniform(0, 2 * np.pi)
+        new_num = np.random.randint(0, 500)
+        while get_dist(new_num, new_rad) < 300:
+            new_num = np.random.randint(0, 500)
+        np.append(new_subject.rads, new_rad)
+        np.append(new_subject.starts, new_angle)
+        np.append(new_subject.nums, new_num)
+        return new_subject
     i = np.random.randint(0, len(subject.rads))
-    feature = np.random.randint(0, 3)
     print("mutate ring", i, end=" ")
-    if feature == 0:
+    if mutation == 'radius':
         print("mutate radius")
-        new_subject.rads[i] += np.random.randint(-new_subject.rads[i], 2001)
-    elif feature == 1:
+        new_rad = np.random.randint(1000, 15000)
+        while get_dist_to_closest_ring(new_rad, new_subject.rads, i) < 300 or get_dist(new_subject.nums[i], new_rad) < 300:
+            new_rad = np.random.randint(1000, 15000)
+        new_subject.rads[i] = new_rad
+    elif mutation == 'angle':
         print("mutate initial angle")
-        new_subject.starts[i] += np.random.uniform(0, 2 * np.pi)
-    else:
+        new_subject.starts[i] = np.random.uniform(0, 2 * np.pi)
+    elif mutation == 'number':
         print("mutate number of particles")
-        new_subject.nums[i] += np.random.randint(-new_subject.nums[i], 100)
+        new_num = np.random.randint(0, 500)
+        while get_dist(new_num, new_subject.rads[i]) < 300:
+            new_num = np.random.randint(0, 500)
+        new_subject.nums[i] = new_num
+    elif mutation == 'remove':
+        print("remove ring")
+        np.delete(new_subject.rads, i)
+        np.delete(new_subject.starts, i)
+        np.delete(new_subject.nums, i)
+
     return new_subject
